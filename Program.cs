71b5e1fd8d92894d933aaca4dbc122c38737d043
roncwynn <rcwynn@gmail.com>
2020-05-13 08:32:47 -0400

    Steps 8 & 9 done

--- Program.cs
+++ Program.cs
@@ -296,6 +296,74 @@ namespace ComputationalThinkingExercises
 
             // Enter your solution here
 
+            Console.WriteLine("Please enter a number in STRING format from ZERO to TEN.");
+            string someNumberString =  Console.ReadLine().ToLower();
+
+            switch (someNumberString)
+            {
+                case "zero":
+                    {
+                        Console.WriteLine("0");
+                        break;
+                    }
+                case "one":
+                    {
+                        Console.WriteLine("1");
+                        break;
+                    }
+                case "two":
+                    {
+                        Console.WriteLine("2");
+                        break;
+                    }
+                case "three":
+                    {
+                        Console.WriteLine("3");
+                        break;
+                    }
+                case "four":
+                    {
+                        Console.WriteLine("4");
+                        break;
+                    }
+                case "five":
+                    {
+                        Console.WriteLine("5");
+                        break;
+                    }
+                case "six":
+                    {
+                        Console.WriteLine("6");
+                        break;
+                    }
+                case "seven":
+                    {
+                        Console.WriteLine("7");
+                        break;
+                    }
+                case "eight":
+                    {
+                        Console.WriteLine("8");
+                        break;
+                    }
+                case "nine":
+                    {
+                        Console.WriteLine("9");
+                        break;
+                    }
+                case "ten":
+                    {
+                        Console.WriteLine("10");
+                        break;
+                    }
+                //case default:
+                //    { }
+                /* default:
+                    {
+                        Console.WriteLine("Invalild Input");
+                    }
+                    */
+            }
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
@@ -313,6 +381,21 @@ namespace ComputationalThinkingExercises
             Console.WriteLine("WHICH NAME IS LONGER?\n");
 
             // Enter your solution here
+
+            //string firstName, lastName;
+
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine();
+            string message = firstName + " " + lastName + ".  ";
+            Console.WriteLine(message);
+
+            if (firstName.Length > lastName.Length) { Console.WriteLine(message + "First is longer."); }
+            else if (firstName.Length == lastName.Length) { Console.WriteLine(message + "Same-sies."); }
+            else { Console.WriteLine(message + "Last must be longer."); }
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
